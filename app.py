88db2cd845b3069e59efbf110cfc52ef212674ce
Lovro <mekis.lovro@gmail.com>
2016-03-26 18:43:43 +0000

    added normal time

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from time import strftime
-from datetime import datetime
+from time import strftime, mktime
+from datetime import datetime, date
 import re
 
 from flask import Flask, render_template, jsonify
@@ -14,7 +14,7 @@ def index():
 def url_input(ext_url):
     
     unix = re.compile("[0-9]{10}")
-    nat = re.compile("(\w+\s\d+\,\s\d{4})")
+    nat = re.compile("\w+\s\d+\,\s\d{4}")
     
     month_names = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
@@ -25,17 +25,25 @@ def url_input(ext_url):
         unix_time = ext_url
         nat_time = datetime.fromtimestamp(float(ext_url)).strftime("%m %d, %Y")
         nat_time = nat_time.replace(nat_time[:2], month_names[int(nat_time[:2]) - 1])
+        
         return jsonify(unix = unix_time, natural = nat_time)
+        
     elif nat.match(ext_url):
         #natural time input case
-        return "natural input"
+        print ext_url
+        date_list = ext_url.split()
+        
+        year = date_list[2]
+        month = month_names.index(date_list[0])
+        day = date_list[1][:len(date_list[1]) - 1]
+        
+        start = date(int(year), int(month), int(day))
+        unix_time = mktime(start.timetuple())
+    
+        return jsonify(unix = unix_time, natural = date_list[0] + " " + str(day) + ", " + str(year) )
+        
     else:
         return jsonify(unix = "null", natural = "null")
-
-    
-    #d = datetime.date(2015,1,5)
-    
-    #unixtime = time.mktime(d.timetuple())
 
 
 ### Runs server
